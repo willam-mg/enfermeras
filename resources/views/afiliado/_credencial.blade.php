64980962149d392e83255e675b03e9b1cd9fb632
willam-gm <will-gm@hotmail.com>
2022-09-23 10:57:12 -0400

    Modificacion de credencial agregar marcas de agua

--- resources/views/afiliado/_credencial.blade.php
+++ resources/views/afiliado/_credencial.blade.php
@@ -35,7 +35,7 @@
         right: -63px;
         letter-spacing: 3px;
         text-transform: uppercase;
-        background: #2ae585;
+        background: #137a45;
         color: #fff;
         padding: 5px 12px 5px 12px;
         transform: rotate(90deg);
@@ -46,7 +46,8 @@
         color: white;
     }
     .text-purple {
-        color: #006691;
+        /* color: #006691; */
+        color: #015980;
     }
     .p-5 {
         padding: 10px;
@@ -104,15 +105,28 @@
         position: absolute;
         bottom: 0;
         
-        width: 321.25984266666666px;
+        /* width: 321.25984266666666px; */
+        width: 100%;
         /* height: 200.31496px; */
-        background-position: center;
+        /* background-position: center; */
         /* background-color: rgb(1, 2, 1, 0.3); */
         /* border: 1px solid black; */
         z-index: 2;
         /* padding-top: 75px; */
         text-align: center;
-        padding-bottom: 70px;
+        padding-bottom: 50px;
+    }
+    .marca-agua-front {
+        display: block;
+        position: absolute;
+        top: 0;
+        width: 100%;
+        z-index: 2;
+        padding-top: 2px;
+        /* padding-left: 15px; */
+        padding-left: 45px;
+        /* text-align: center; */
+        /* padding-left: 10px; */
     }
 
     /* @page { 
@@ -124,39 +138,44 @@
 <div class="border-styled">
     <div class="border-styled-white"></div>
 </div>
+<div class="marca-agua-front">
+    <img src="{{public_path('img/isotipo-marcaagua.png')}}" alt="foto" width="265" height="auto">
+</div>
 <div class="wrapper-card">
     <table class="table table-light full-width" height="200.31496" border="1">
         <tbody>
             <tr>
                 <td colspan="3" class="text-center" style="padding:5px">
-                    <strong class="text-uppercase">{{$model->nombre_completo}}</strong>
+                    <strong class="text-uppercase" style="font-size: 8pt">{{$model->nombre_completo}}</strong>
                 </td>
             </tr>
             <tr>
                 <td class="text-center bg-gray-2" style="width: 150px">
-                    <b class="text-purple">
+                    <b class="text-purple text-uppercase">
                         {{$model->cargo}}
                     </b>
                 </td>
                 <td rowspan="7" class="text-center">
                     @if ($model->foto)
-                        <img src="{{public_path('uploads/thumbnail/'.$model->src_foto)}}" class="" alt="foto" width="100" height="100">
+                        <img src="{{public_path('uploads/'.$model->src_foto)}}" alt="foto" width="100" height="100" style="border:0.8px solid black">
                     @else
-                        <img src="{{public_path('img/img_user_none.png')}}" alt="foto" width="110" height="110">
+                        <img src="{{public_path('img/img_user_none.png')}}" alt="foto" width="100" height="100" style="border:0.8px solid black">
                     @endif
                 </td>
-                <td rowspan="8" style="width: 18px">
+                <td rowspan="8" style="width: 18px;">
                     <div class="aside">
                         {{__('ENFERMERÍA')}}
                     </div>
                 </td>
             </tr>
             <tr>
-                <td class="text-end">C.I.:</td>
+                <td class="text-end">{{__('Fecha de registro: ')}}</td>
             </tr>
             <tr>
                 <td class="text-end bg-gray-1 text-purple"> 
-                    {{$model->ci}} 
+                    <b>
+                        {{$model->fecha_registro}} 
+                    </b>
                 </td>
             </tr>
             <tr>
@@ -166,32 +185,43 @@
             </tr>
             <tr>
                 <td class="text-end bg-gray-1 text-purple">
-                    {{$model->numero_matricula}}
+                    <b>
+                        {{$model->numero_matricula}}
+                    </b>
                 </td>
             </tr>
             <tr>
                 <td class="text-end">
-                    {{__('Numero de afiliado: ')}}
+                    {{__('C.I.: ')}}
                 </td>
             </tr>
             <tr>
                 <td class="text-end bg-gray-2  text-purple">
+                    <b>
+                        {{$model->ci}}
+                    </b>
+                </td>
+            </tr>
+            <tr>
+                <td rowspan="2" class="text-center text-uppercase">
+                    <img src="{{public_path('img/isotipo.svg')}}" alt="foto" width="45" height="auto">
+                    <br>
+                    <b>
+                        {{__('A.D.E.A. CBBA')}}
+                    </b>
+                    <div style="font-size: 4pt; letter-spacing: 0; word-spacing: 0; line-height : 6px; font-stretch: extra-condensed;">
+                        {{__('Asociación Departamental de Enfermeras(os)')}} <br>
+                        {{__('Auxiliares de Cochabamba')}}
+                    </div>
+                </td>
+                <td class="text-center" style="padding: 0; height: 20px; font-size: 11pt; font-weight: bold; line-height : 7px">
                     {{$model->numero_afiliado}}
+                    <br>
+                    <span style="font-size: 4pt; font-weight: normal">{{__('Numero de afiliado')}}</span>
                 </td>
             </tr>
             <tr>
-                <td rowspan="2" class="text-center text-white">
-                    <img src="{{public_path('img/logo-landscape.png')}}" alt="foto" width="70" height="auto">
-                    <p style="font-size: 5pt">
-                        {{__('Asociacion Departamental de Enfermeras(os) Auxiliares A.D.E.A. - Cochabamba')}}
-                    </p>
-                </td>
-                <td class="text-center" style="height: 25px; padding-left: 24px">
-                    <?=DNS1D::getBarcodeHTML($model->ci, 'EAN13',1,20)?>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" class="text-center text-white" style="padding:13px 2px 0 0;font-size:5pt">
+                <td colspan="2" class="text-center" style="padding:5px 2px 0 0;font-size:5pt">
                     {{__('Personeria juridica Nro 204770 - Resolucion Prefectural Nro 135/05')}}
                 </td>
             </tr>
@@ -205,19 +235,44 @@
         <tbody>
             <tr>
                 <td colspan="3">
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                    Fecha de validez:
+                    <b>
+                        {{$model->fecha_registro}} - 
+                        {{$model->fecha_registro}}
+                    </b>
                 </td>
             </tr>
             <tr>
-                <td rowspan="2" style="padding: 10px 0 10px 25px ">
-                    <?=DNS2D::getBarcodeHTML($model->numero_afiliado, 'QRCODE', 3, 3)?>
-                    
+                <td colspan="3">
+                    Egreso:
+                    <b class="text-uppercase">
+                        {{$model->egreso}}
+                    </b>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    {{__('Años de servicio: ')}}
+                    <b>
+                        {{$model->anos_servicio}}
+                    </b>
+                </td>
+                <td colspan="2">
+                    {{__('Grupo sanguineo: ')}}
+                    <b>
+                        {{$model->grupo_sanguineo}}
+                    </b>
+                </td>
+            </tr>
+            <tr>
+                <td rowspan="2" style="padding: 2px 0 5px 10px ">
+                    <?=DNS2D::getBarcodeHTML($model->numero_afiliado, 'QRCODE', 3.5, 3.5)?>
                 </td>
                 <td colspan="2" rowspan="2">
                     <p>
                         {{__('En caso de extravio de esta identificacion llamar al: ')}}
                     </p>
-                    {{__('45645655 - 5675767 Asociacion Departamental de Enfermeras(os) Auxiliares')}}
+                    {{__('4317867')}}
                 </td>
             </tr>
             <tr>
@@ -225,11 +280,13 @@
                 </td>
             </tr>
             <tr>
-                <td colspan="2">
-                    {{__('Asociacion Departamental de Enfermeras(os) Auxiliares Tlf: 45645655 - 5675767 Direccion: av quillas nro23')}}
+                <td colspan="2" style="font-size: 6pt">
+                    {{__('Asociacion Departamental de Enfermeras(os) Auxiliares ')}} <br>
+                    {{__('Teléfono: 4317867')}} <br>
+                    {{__('Direccion: C. Colombia entre C. 16 de julio y Av Oquendo Edif. ----- Piso --- Oficina -- ')}}
                 </td>
-                <td>
-                    <img src="{{public_path('img/logo-landscape.png')}}" alt="foto" width="70" height="auto">
+                <td class="text-center">
+                    <img src="{{public_path('img/logo-v2.svg')}}" alt="foto" width="70" height="auto">
                 </td>
             </tr>
         </tbody>
@@ -238,5 +295,5 @@
 <div class="border-styled-back">
 </div>
 <div class="marca-agua">
-    <img src="{{public_path('img/logo-marca-agua.png')}}" alt="foto" width="100" height="auto">
+    <img src="{{public_path('img/logo-redondo-marcaagua.png')}}" alt="foto" width="120" height="auto">
 </div>
